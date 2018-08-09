c85d7777fca10b34f6cfe7ff9af91c31b70c1432
Celso Mamani <celsinho.mch@gmail.com>
2018-08-09 15:35:42 -0400

    Tercera version, se corrigio las el archivo de rutas y se eliminaron las fotos

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/', function () {
 
 Route::resource('estudiantes','EstudianteController');
 
-Route::apiResource('api/estudiantes','EstudianteControllerApi');
-
-
+Route::apiResource('api/estudiantes','EstudianteControllerApi',[
+    'as' => 'api'
+]);
 
 //Route::name('print')->get('/imprimir', 'GeneradorController@imprimir');
